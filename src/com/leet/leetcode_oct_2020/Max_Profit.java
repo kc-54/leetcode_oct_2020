647f23b0f40c1378d27c0b2f9aeace08ccf55986
stupidDummy <kelvinchandra54@yahoo.com>
2020-10-20 13:28:39 +0800

    leetcode 18/10/20 [Updated]Best Time To Buy and Sell Stock IV

--- src/com/leet/leetcode_oct_2020/Max_Profit.java
+++ src/com/leet/leetcode_oct_2020/Max_Profit.java
@@ -5,16 +5,147 @@ public class Max_Profit {
 	
 	public static void main (String [] args) {
 		int k = 2;
-//		int [] prices = {2,6,8,7,8,7,9,4,1,2,4,5,8,2,9};
+		
+		//test cases
+		int [] prices = {2,6,8,7,8,7,9,4,1,2,4,5,8,2,9};
 //		int [] prices = {8,6,4,3,3,2,3,5,8,3,8,2,6};
 //		int [] prices = {9,5,7,4,2,4,1,6,4};
 //		int [] prices = {3,4,6,0,3,7,5,8,2,9,1,6,6,2};
-		int [] prices = {6,1,3,2,4,7};
+//		int [] prices = {6,1,3,2,4,7};
 		
-		System.out.println(maxProfit(k,prices));
+		System.out.println(maxProfitWithMerging(k,prices));
+		System.out.println(maxProfitWithDP(k,prices));
+		System.out.println(maxProfitWithDP_Compressed_Array(k,prices));
 	}
 	
-	public static int maxProfit(int k, int[] prices) {
+	//dp solution with state array compressed
+	//Runtime 2ms, Memory 39.2MB
+	//Status Accepted
+    public static int maxProfitWithDP_Compressed_Array(int k, int[] prices) {
+    	
+        int n = prices.length;
+        
+        if(k<=0 || n<=0) {
+        	return 0;
+        }
+
+        if(n < 2*k) {
+        	int rs = 0;
+        	for (int i = 1; i < n; i++) {
+        		rs += Math.max(0, prices[i]-prices[i-1]);
+        	}
+        	return rs;
+        	
+        }
+        
+        //dp[i][j][k]
+        //i for ith transactions
+        //j for number of transaction used
+        //k 0 for not hold stock, 1 for hold stock
+        int [][][] dp = new int[2][k+1][2];
+
+        // initialize the array with -inf
+        // we use -1e9 here to prevent overflow
+        for(int i = 0; i < 2; i++) {
+        	for(int j = 0; j <= k; j++) {
+        		dp[i][j][0] = -1000000000;
+        		dp[i][j][1] = -1000000000;
+        	}
+        }
+        
+        //start value
+        dp[0][0][0] = 0;
+        dp[0][1][1] = -prices[0];
+
+        for(int i = 1; i < n; i++) {
+        	for(int j = 0; j <= k; j++) {
+        		
+        		//transition value calculation
+        		dp[1][j][0] = Math.max(dp[0][j][0], dp[0][j][1] + prices[i]);
+        		//can't hold stock without any transaction
+        		if(j>0) {
+        			dp[1][j][1] = Math.max(dp[0][j][1], dp[0][j-1][0]-prices[i]);
+        		}
+        	}
+        	//replace previous result with the latest as the previous result is useless
+        	for(int j = 0; j <= k; j++) {
+        		dp[0][j][0] = dp[1][j][0];
+        		dp[0][j][1] = dp[1][j][1];
+        	}
+        	
+        }
+
+    	int rs = 0;
+        for(int i = 0; i <= k; i++) {
+        	rs = Math.max(rs, dp[1][i][0]);
+        }
+        
+    	return rs;
+    }
+	
+    //dp basic solution
+	//Runtime 5ms, Memory 40.8MB
+	//Status Accepted
+	public static int maxProfitWithDP(int k, int[] prices) {
+		int n = prices.length;
+        
+        if(k<=0 || n<=0) {
+        	return 0;
+        }
+
+        if(n < 2*k) {
+        	int rs = 0;
+        	for (int i = 1; i < n; i++) {
+        		rs += Math.max(0, prices[i]-prices[i-1]);
+        	}
+        	return rs;
+        	
+        }
+        
+        
+        //dp[i][j][k]
+        //i for ith transactions
+        //j for number of transaction used
+        //k 0 for not hold stock, 1 for hold stock
+        int [][][] dp = new int[n][k+1][2];
+
+        // initialize the array with -inf
+        // we use -1e9 here to prevent overflow
+        for(int i = 0; i < n; i++) {
+        	for(int j = 0; j <= k; j++) {
+        		dp[i][j][0] = -1000000000;
+        		dp[i][j][1] = -1000000000;
+        	}
+        }
+        
+        //start value
+        dp[0][0][0] = 0;
+        dp[0][1][1] = -prices[0];
+
+        for(int i = 1; i < n; i++) {
+        	for(int j = 0; j <= k; j++) {
+        		//transition value calculation
+        		dp[i][j][0] = Math.max(dp[i-1][j][0], dp[i-1][j][1] + prices[i]);
+        		//can't hold stock without any transaction
+        		if(j>0) {
+        			dp[i][j][1] = Math.max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i]);
+        		}
+        	}
+        }
+
+    	int rs = 0;
+        for(int i = 0; i <= k; i++) {
+        	rs = Math.max(rs, dp[n-1][i][0]);
+        }
+        
+    	return rs;
+	}
+	
+	
+	//inefficent way, merging with brute force and recursion
+	//Runtime N/A, Memory N/A
+	//Status Time Limit Exceeded
+	public static int maxProfitWithMerging(int k, int[] prices) {
 		
         int rs = 0;
         
@@ -48,9 +179,6 @@ public class Max_Profit {
 			counter++;
 		}
 		rs = findMaxWithK(prices, mins, maxs, k);
-		for(int i = 0; i < counter; i++) {
-			System.out.println("a pos " + mins[i] + " = " +prices[mins[i]] + " pos " + maxs[i] + " = " +prices[maxs[i]]);
-		}
 		for(int i = 0; i < counter-1; i++) {
 			int temp = findMax(prices,mins,maxs,i,counter,k);
 			rs = rs>temp?rs:temp;
@@ -62,17 +190,8 @@ public class Max_Profit {
 	
 	public static int findMax(int[] prices, int[] min, int [] max, int pos, int limit, int k) {
 		int rs = 0;
-		System.out.println();
-		System.out.println(" entered details ");
-		System.out.println("pos " + pos);
-		System.out.println("arrays : ");
-
-		for(int i = 0; i < limit; i++) {
-			System.out.println("x pos " + min[i] + " = " +prices[min[i]] + " pos " + max[i] + " = " +prices[max[i]]);
-		}
 		
 		if(pos < min.length && prices[min[pos]] < prices[min[pos+1]] && prices[max[pos]] <= prices[max[pos+1]]) {
-			System.out.println("satisfied");
 			int[] temp_min = new int[limit-1];
 			int[] temp_max = new int[limit-1];
 			int pointer = 0;
@@ -88,17 +207,10 @@ public class Max_Profit {
 				}
 				pointer++;
 			}
-			System.out.println("new array");
-			for(int i = 0; i < limit-1; i++) {
-				System.out.println("x pos " + temp_min[i] + " = " +prices[temp_min[i]] + " pos " + temp_max[i] + " = " +prices[temp_max[i]]);
-			}
 
 			rs = findMaxWithK(prices, temp_min, temp_max, k);
-			System.out.println("rs from new array = " + rs);
 			for(int i = pos; i < temp_min.length-1; i++) {
-				System.out.println("entering recursion");
 				int curr = findMax(prices,temp_min, temp_max, i, limit-1, k);
-				System.out.println("leaving recursion, curr rs = " +rs + " obtained rs = " +curr + " cur pos " + i + " " + (temp_min.length-1));
 				
 				rs = rs>curr?rs:curr;
 			}
@@ -107,7 +219,6 @@ public class Max_Profit {
 			rs = rs>temp_rs?rs:temp_rs;
 			
 		}
-		System.out.println("return rs = " + rs);
 		
 		return rs;
 	}
@@ -146,56 +257,4 @@ public class Max_Profit {
 		}
 		return list;
 	}
-//	if(k >= counter) {
-//	for(int i = 0; i < counter; i++) {
-//		rs += (prices[maxs[i]] - prices[mins[i]]);
-//	}
-//}else {
-//	System.out.println("counter " + counter);
-//	int temp_counter = counter;
-//	int[] temp_mins = new int[counter];
-//	int[] temp_maxs = new int[counter];
-//	int[] temp_diffs = new int[counter];
-//	int temp_i = 0;
-//	
-//	
-//	for(int i = 0; i < counter; i++) {
-//		
-//		temp_mins[temp_i] = mins[i];
-//		temp_maxs[temp_i] = maxs[i];
-//		
-//		System.out.println("test test " + i + " " + temp_i);
-//		
-//		while(k < temp_counter && prices[mins[temp_i]] < prices[mins[i+1]] && prices[maxs[temp_i]] <= prices[maxs[i+1]]) {
-//
-//			if(i>=counter-1)break;
-////			int [] prices = {9,5,7,4,2,4,1,6,4};
-//			System.out.println("entered " + temp_i);
-//			System.out.println("test 2a " + mins[temp_i] + " " + mins[i+1]+ " b " + maxs[temp_i]+ " " +  maxs[i+1]);
-//			System.out.println("test a " + prices[mins[temp_i]] + " " + prices[mins[i+1]] + " b " + prices[maxs[temp_i]] + " " +  prices[maxs[i+1]]);
-//			if(temp_counter == k)break;
-//			
-//			temp_mins[temp_i] = mins[temp_i];
-//			temp_maxs[temp_i] = maxs[i+1];
-//			i++;
-//			temp_counter--; 
-//			
-//		}
-//		temp_diffs[temp_i] = prices[temp_maxs[temp_i]] - prices[temp_mins[temp_i]];
-//		temp_i ++;
-//	}
-//	
-//	System.out.println("temp " + temp_counter);
-//	if(temp_counter != k) {
-//		temp_diffs = sort_array(temp_diffs);
-//		for(int i : temp_diffs) {
-//			System.out.println( " test " + i);
-//		}
-//	}
-//	for(int i = 0; i < k; i++) {
-//		System.out.println("b pos " + temp_mins[i] + " = " +prices[temp_mins[i]] + " pos " + temp_maxs[i] + " = " + + prices[temp_maxs[i]] );
-//		rs += temp_diffs[i];
-//	}
-//	
-//}
 }
